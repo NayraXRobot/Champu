27415efb4f896f46319d3f9867dbd44ab1b1392a
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-08-22 19:58:07 +0530

    Update userbot.py

--- ChampuXMusic/core/userbot.py
+++ ChampuXMusic/core/userbot.py
@@ -65,10 +65,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TheShivanshu")
-                await self.one.join_chat("TheChampuClub")
-                await self.one.join_chat("akaChampu")
-                await self.one.join_chat("TheChampu")
+                await self.five.join_chat("AboutshayarAkhawab")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
 
             except:
                 pass
@@ -95,10 +95,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TheShivanshu")
-                await self.two.join_chat("TheChampuClub")
-                await self.two.join_chat("akaChampu")
-                await self.two.join_chat("TheChampu")
+                await self.five.join_chat("AboutshayarAkhawab")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
             except:
                 pass
             assistants.append(2)
@@ -119,10 +119,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TheShivanshu")
-                await self.three.join_chat("TheChampuClub")
-                await self.three.join_chat("akaChampu")
-                await self.three.join_chat("TheChampu")
+                await self.five.join_chat("AboutshayarAkhawab")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
             except:
                 pass
             assistants.append(3)
@@ -144,10 +144,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TheShivanshu")
-                await self.four.join_chat("TheChampuClub")
-                await self.four.join_chat("akaChampu")
-                await self.four.join_chat("TheChampu")
+                await self.five.join_chat("AboutshayarAkhawab")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
             except:
                 pass
             assistants.append(4)
@@ -169,10 +169,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TheShivanshu")
-                await self.five.join_chat("TheChampuClub")
-                await self.five.join_chat("akaChampu")
-                await self.five.join_chat("TheChampu")
+                await self.five.join_chat("AboutshayarAkhawab")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
+                await self.five.join_chat("ll_ZIDDI_SHAYAR_ll")
             except:
                 pass
             assistants.append(5)
