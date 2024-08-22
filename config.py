a4da142b094f57ebc17b4a31cdf91bdb8abd1989
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-08-22 19:59:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NayraXRobot/champuMusic",
+    "https://github.com/NayraXRobot/Champu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
